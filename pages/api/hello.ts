d85bd246e9f253d1176b8092196dcad4e356712b
danielsitio <danest.esc@gmail.com>
2024-05-13 18:39:37 -0300

    data test

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { ICommonTagsResult, parseFile } from 'music-metadata'
 
-type Data = {
-  name: string;
-};
+type Song = {
+  title: string
+  duration: number,
+}
+type a = ICommonTagsResult
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>,
-) {
-  res.status(200).json({ name: "John Doe" });
+export default function handler(req: NextApiRequest, res: NextApiResponse<ICommonTagsResult>) {
+  parseFile("./assets/Mike Schpitz and Phys Edison - Sly & The Family Jones.mp3", { duration: true })
+    .then(({ common, format, native, quality }) => {
+      const { duration, } = format
+      const { title, album, picture } = common!
+      res.status(200).json(common!)
+    })
+    .catch(e => console.log("error : " + e.message))
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,37 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
+import { GetStaticProps } from 'next'
+import { ICommonTagsResult } from "music-metadata";
+import { useEffect, useState } from "react";
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
+
+type props = {
+  song: ICommonTagsResult
+}
+
+export default function Home({ song }: props) {
+
+  const [imgSrc, setImgSrc] = useState("")
+
+  const imgData = song.picture![0].data["data"]
+
+  useEffect(() => {
+
+    let data: string = ""
+    for (var i = 0; i < imgData.length; i++) {
+      data += String.fromCharCode(imgData[i]);
+    }
+
+    setImgSrc(`data:image/jpeg;base64,${btoa(data)}`)
+    return () => {
+
+    }
+  }, [])
+
+
   return (
     <>
       <Head>
@@ -14,101 +40,23 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{" "}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-    </>
-  );
+      <div>el titulo es {song.title}</div>
+      <div>el formato de la imagen es {song.picture![0].format}</div>
+      <Image src={imgSrc} alt="image" width={350} height={350} />
+    </>)
 }
+
+
+
+export const getStaticProps = (async (context) => {
+  const res = await fetch("http://localhost:3000/api/hello")
+  const data: ICommonTagsResult = await res.json()
+
+  return {
+    props: {
+      song: data
+    }
+  }
+}) satisfies GetStaticProps<{
+  song: ICommonTagsResult
+}>
